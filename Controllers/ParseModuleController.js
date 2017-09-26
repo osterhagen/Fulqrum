37618c02ce9c7f9183033eefab410fc3a9b9f469
Connor Tiger Borzello <connorborz@gmail.com>
2017-09-25 21:37:36 -0400

    Added the controller instruction for a fresh scrape when no previous analytics are found

--- Controllers/ParseModuleController.js
+++ Controllers/ParseModuleController.js
@@ -1,6 +1,19 @@
 var WebScraper = require("../Models/ParseModule/WebScraper.js");
 
 exports.main = function main(app) {
+    //Method expects a request specifying parameters to search for
+    app.post("/scrape", function (request, response) {
+        //Subject to change based on how we want to receive the company name
+        var companyName = request.body["CompanyName"];
 
+        var hasPriorAnalytics = false;
+        //TODO Check if company analytics are already in database
+
+        if (hasPriorAnalytics === false) {
+            WebScraper.freshScrape(companyName);
+        } else {
+            //TODO call WebScraper Method for when analytics already exist
+        }
+    });
 
 }
