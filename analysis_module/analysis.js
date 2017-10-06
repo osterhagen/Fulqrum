4c423528ceade00099af646c382aca2e09fa9e53
ThomasHuang <ThomasHuang@purdue.edu>
2017-10-06 02:17:06 -0700

    cleaning up files

--- analysis_module/analysis.js
+++ analysis_module/analysis.js
@@ -1,34 +1,10 @@
 #! /usr/bin/env node
 
-function analyzeSentimentOfText (text) {
-  // [START language_sentiment_string]
 function analyzeSentences (auth_fp, file) {
   // Imports the Google Cloud client library
   const Language = require('@google-cloud/language');
 
   // Instantiates a client
-  const language = Language();
-
-  // The text to analyze, e.g. "Hello, world!"
-  // const text = 'Hello, world!';
-
-  const document = {
-    'content': text,
-    type: 'PLAIN_TEXT'
-  };
-
-  console.log('starting');
-
-  // Detects the sentiment of the document
-  language.analyzeSentiment({ document: document })
-    .then((results) => {
-      const sentiment = results[0].documentSentiment;
-      console.log(`Document sentiment:`);
-      console.log(`  Score: ${sentiment.score}`);
-      console.log(`  Magnitude: ${sentiment.magnitude}`);
-
-      const sentences = results[0].sentences;
-      sentences.forEach((sentence) => {
   const language = Language({
     keyFilename: auth_fp
   });
@@ -80,52 +56,6 @@ function analyzeSentences (auth_fp, file) {
     .catch((err) => {
       console.error('ERROR:', err);
     });
-  // [END language_sentiment_string]
-}
-
-function analyzeEntitiesOfText (text) {
-  // [START language_entities_string]
-  // Imports the Google Cloud client library
-  const Language = require('@google-cloud/language');
-
-  // Instantiates a client
-  const language = Language();
-
-  // The text to analyze, e.g. "Hello, world!"
-  // const text = 'Hello, world!';
-
-  // Instantiates a Document, representing the provided text
-  const document = {
-    'content': text,
-    type: 'PLAIN_TEXT'
-  };
-
-  // Detects entities in the document
-  language.analyzeEntities({ document: document })
-    .then((results) => {
-      const entities = results[0].entities;
-
-      console.log('Entities:');
-      entities.forEach((entity) => {
-        console.log(entity.name);
-        console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
-        if (entity.metadata && entity.metadata.wikipedia_url) {
-          console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
-        }
-      });
-    })
-    .catch((err) => {
-      console.error('ERROR:', err);
-    });
-  // [END language_entities_string]
-}
-
-const argv = require('yargs').argv;
-
-if(argv.sentimenttxt) {
-  analyzeSentimentOfText(argv.sentimenttxt);
-} else if (argv.entitytxt) {
-  analyzeEntitiesOfText(argv.entitytxt);
     //[END analyzeSentiment]
 }
 
