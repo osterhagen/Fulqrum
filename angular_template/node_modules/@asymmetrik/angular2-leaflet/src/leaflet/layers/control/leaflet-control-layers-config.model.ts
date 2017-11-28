5ddc188f7da2b35176a3f8f604b6a15caa9e7644
ThomasHuang <ThomasHuang@purdue.edu>
2017-11-27 19:11:18 -0500

    dropped unnecessary node modules

--- angular_template/node_modules/@asymmetrik/angular2-leaflet/src/leaflet/layers/control/leaflet-control-layers-config.model.ts
+++
@@ -1,8 +0,0 @@
-import * as L from 'leaflet';
-
-export class LeafletControlLayersConfig {
-	constructor(
-		public baseLayers?: L.Control.LayersObject,
-		public overlays?: L.Control.LayersObject
-	) { }
-}
--- angular_template/node_modules/@nebular/auth/services/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth.service';
-export * from './token.service';
-export * from './interceptors/jwt-interceptor';
-export * from './interceptors/simple-interceptor';
